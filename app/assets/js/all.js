8a25dd44b8ecfacabe0eeddebd83c323cbf9ad68
huanginch <huanginch329@gmail.com>
2022-09-11 13:14:36 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".artist-swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: -50,
+    depth: 200,
+    modifier: 6,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+$(document).ready(function () {
+  $('.navbar__input__icon').click(function (e) {
+    e.preventDefault();
+    $('.navbar__input').toggleClass('show');
+  });
+})
