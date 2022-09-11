708e82bd2f4174685b7327165e21bf0bbfa921a0
huanginch <huanginch329@gmail.com>
2022-09-11 13:16:06 +0800

    Update 2022-09-11T05:15:59.133Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var swiper = new Swiper(".artist-swiper", {
   effect: "coverflow",
   grabCursor: true,
@@ -8,22 +10,22 @@ var swiper = new Swiper(".artist-swiper", {
     stretch: -50,
     depth: 200,
     modifier: 6,
-    slideShadows: false,
+    slideShadows: false
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable: true
   },
   breakpoints: {
     992: {
-      slidesPerView: 2,
-    },
-  },
+      slidesPerView: 2
+    }
+  }
 });
-
 $(document).ready(function () {
   $('.navbar__input__icon').click(function (e) {
     e.preventDefault();
     $('.navbar__input').toggleClass('show');
   });
-})
+});
+//# sourceMappingURL=all.js.map
